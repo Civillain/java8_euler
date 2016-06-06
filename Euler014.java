ecdb97f6e015def7df46c6a88a2e39e5eb508c1a
Civillain <reinout.korbee@gmail.com>
2016-06-06 12:43:27 +0200

    Create Euler014.java

---
+++ Euler014.java
@@ -0,0 +1,40 @@
+package problems;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+/**
+ * @author rko
+ *
+ */
+public class Euler014 {
+	final int max = 1000000;
+	long next = 0;
+	long maxPathLength = 0;
+	long startPos = 0;
+	
+	
+	@Test
+	public void test() {
+		int pathLength = Stream.iterate(2l, n -> n + 1).limit(max)
+			.map(n -> {
+				int length = 1; // start at 1 because we're not counting the last element in the sequence, namely 1
+				while(n > 1) {  // the last element is not counted and is used as stop condition
+					if( (n % 2) == 0) {
+						n = n / 2;
+					} else {
+						n = (n * 3) + 1;
+					}
+					length++;
+				}
+				return length;
+			})
+			.reduce(0, (a,b) -> a > b ? a : b);
+		
+		assertEquals(525, pathLength);
+		System.out.println("max path: " + pathLength);
+	}
+}
